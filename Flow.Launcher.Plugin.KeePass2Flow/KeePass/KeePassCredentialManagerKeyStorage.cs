de4139a4d8cb65ac5358abfc41f332e8a6e50747
geisterfurz007 <michelvonv@me.com>
2024-05-03 23:27:23 +0200

    feat: use windows credential manager for storing the key

---
+++ Flow.Launcher.Plugin.KeePass2Flow/KeePass/KeePassCredentialManagerKeyStorage.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using AdysTech.CredentialManager;
+using Keepass2Client.Setup;
+
+namespace Flow.Launcher.Plugin.KeePass2Flow.KeePass;
+
+public class KeePassCredentialManagerKeyStorage : KeePassKeyStorage
+{
+    private const string CredentialsKeyBase = "KeePass2Flow";
+    private string CredentialsKey => CredentialsKeyBase + "-" + Username;
+    
+    public KeePassCredentialManagerKeyStorage(string username) : base(username)
+    {
+    }
+
+    protected override Task<string> GetStoredKeyAsync()
+    {
+        var credential = CredentialManager.GetCredentials(CredentialsKey);
+        if (credential is null || credential.UserName != Username) throw new KeyNotFoundException();
+
+        return Task.FromResult(credential.Password);
+    }
+
+    public override Task StoreKeyAsync(string key)
+    {
+        var credential = new NetworkCredential {Password = key, UserName = Username};
+        CredentialManager.SaveCredentials(CredentialsKey, credential);
+
+        return Task.CompletedTask;
+    }
+}
+
+public class KeyNotFoundException : Exception {}
